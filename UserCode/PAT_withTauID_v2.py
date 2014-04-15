1f5f33c5afa1a33e00c9c0026a053e138482b64b
Shalhout Shalhout <shalhout@pb-d-128-141-111-49.cern.ch>
2014-04-15 15:52:06 +0200

    debugging MVA MET

--- UserCode/PAT_withTauID_v2.py
+++ UserCode/PAT_withTauID_v2.py
@@ -135,34 +135,53 @@ process.tauSequence = cms.Path(process.Step1VertexPresent *
 # recoil corrections will be done at ntuple stage (I think ..)
 #########################
 
+process = cms.Process("analysis")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10)
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
+process.load('JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff')
+
+#process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+#process.load('pharris.MVAMet.metProducerSequence_cff')
+#process.load('JetMETCorrections.METPUSubtraction.pileupJetIdMVASequence_cff')
+
+#process.GlobalTag.globaltag = 'GR_R_42_V23::All'
+#process.GlobalTag.globaltag = 'MC_44_V12::All'
+#process.GlobalTag.globaltag = 'MC_44_V12::All'
+process.GlobalTag.globaltag = 'START53_V15::All'
+
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+#process.source = cms.Source("PoolSource",
+#                            fileNames = cms.untracked.vstring(
+#    '/store/cmst3/user/pharris/HTauTauSynchronization/VBF_HToTauTau_M-120_8TeV-powheg-pythia6-tauola_22612DED-8F8A-E111-92A1-0017A4770034.root'
+    #'/store/cmst3/user/pharris/HTauTauSynchronization/VBF_HToTauTau_M-120_8TeV-powheg-pythia6-tauola_FED5F7FE-0597-E111-BE71-485B39800BB5.root'
+#'root://cmsxrootd-site.fnal.gov//store/mc/Summer12_DR53X/GluGluToHToTauTau_M-125_8TeV-powheg-pythia6/AODSIM/PU_S10_START53_V#7A-v1/0000/00E903E2-9FE9-E111-8B1E-003048FF86CA.root'
+
+#                            ),
+#                            skipEvents = cms.untracked.uint32(0)
+#)
+
+process.output = cms.OutputModule("PoolOutputModule",
+                                  outputCommands = cms.untracked.vstring('keep *')
+#                                  fileName = cms.untracked.string("test.root")
+                                  )
+
+process.ana      = cms.Sequence(process.pfMEtMVAsequence)
+process.p        = cms.Path(process.ana)
+process.outpath  = cms.EndPath(process.output)
+
+### To add the Jet Id
+#+process.pileupJetIdProducer)
 
 
-#process = cms.Process("mvamet")
-#process.load("FWCore.MessageService.MessageLogger_cfi")
-#process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10)
-#process.load('Configuration.StandardSequences.Services_cff')
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
-#process.load('JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff')
-#process.GlobalTag.globaltag = 'START53_V15::All'
 
 
-#process.output = cms.OutputModule("PoolOutputModule",
-#                                  outputCommands = cms.untracked.vstring('keep *')
-#                                  )
-
-#process.ana      = cms.Sequence(process.pfMEtMVAsequence)
-#process.p        = cms.Path(process.ana)
-
-# need to figure out how to get  this in : +process.pileupJetIdProducer
-
-#process.outpath  = cms.EndPath(process.output)
-
-
-
-
-
-
+#########################
+######
+######
 #########################
 
 process.out.outputCommands +=['keep *_patConversions*_*_*']
@@ -175,6 +194,7 @@ process.out.outputCommands +=['keep *_offlinePrimaryVertices*_*_*']
 
 process.out.SelectEvents.SelectEvents = ['tauSequence']
 
+process.out.fileName = 'patTuple_topSelection.root'
 #########################
 
 
@@ -184,7 +204,7 @@ process.source.fileNames=['root://cmsxrootd-site.fnal.gov//store/mc/Summer12_DR5
 
 process.maxEvents.input = 100
 
-process.out.fileName = 'patTuple_topSelection.root'
+
 
 #process.options.wantSummary = False
 #
