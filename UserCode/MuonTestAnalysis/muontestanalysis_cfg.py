2dba524afe9a3fdc3a997ba48062f14923f83f61
Shalhout Shalhout <sshalhou@cern.ch>
2014-04-15 18:32:41 +0200

    fixing issue with source

--- UserCode/MuonTestAnalysis/muontestanalysis_cfg.py
+++ UserCode/MuonTestAnalysis/muontestanalysis_cfg.py
@@ -10,12 +10,12 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 
-
-
 process.source = cms.Source("PoolSource",
-                          cms.untracked.vstring('file:/uscms/home/shalhout/1stSteps/Git2/'+
-                          'DemoTauCode/CMSSW_5_3_14/src/patTuple_testing.root')
-                          )
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        'file:/uscms/home/shalhout/1stSteps/Git2/DemoTauCode/CMSSW_5_3_14/src/patTuple_testing.root'
+    )
+)
 
 
 process.demo = cms.EDAnalyzer('MuonTestAnalysis',
