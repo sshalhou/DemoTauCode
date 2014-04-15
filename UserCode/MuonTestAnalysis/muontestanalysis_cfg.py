a9a69d8621c125b4050a567a9b2df2e508925afe
Shalhout Shalhout <sshalhou@cern.ch>
2014-04-15 18:10:33 +0200

    deleting line

--- UserCode/MuonTestAnalysis/muontestanalysis_cfg.py
+++ UserCode/MuonTestAnalysis/muontestanalysis_cfg.py
@@ -3,8 +3,14 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+
+
 
 process.source = cms.Source("PoolSource",
                           cms.untracked.vstring('file:/uscms/home/shalhout/1stSteps/Git2/'+
