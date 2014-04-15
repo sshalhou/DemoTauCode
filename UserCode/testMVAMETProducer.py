0bf8eeace817a0fabf25a6ca7dca0d827f800876
Shalhout Shalhout <shalhout@pb-d-128-141-111-13.cern.ch>
2014-04-15 11:36:22 +0200

    fixed file source

--- UserCode/testMVAMETProducer.py
+++ UserCode/testMVAMETProducer.py
@@ -17,20 +17,22 @@ process.load('JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff')
 #process.GlobalTag.globaltag = 'MC_44_V12::All'
 process.GlobalTag.globaltag = 'START53_V15::All'
 
-
-
-############################
-process.source.fileNames=['root://cmsxrootd-site.fnal.gov//store/mc/Summer12_DR53X/GluGluToHToTauTau_M-125_8TeV-powheg-pythia6/AODSIM/PU_S10_START53_V7A-v1/0000/00E903E2-9FE9-E111-8B1E-003048FF86CA.root']
-
-
-
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+#    '/store/cmst3/user/pharris/HTauTauSynchronization/VBF_HToTauTau_M-120_8TeV-powheg-pythia6-tauola_22612DED-8F8A-E111-92A1-0017A4770034.root'
+    #'/store/cmst3/user/pharris/HTauTauSynchronization/VBF_HToTauTau_M-120_8TeV-powheg-pythia6-tauola_FED5F7FE-0597-E111-BE71-485B39800BB5.root'
+'root://cmsxrootd-site.fnal.gov//store/mc/Summer12_DR53X/GluGluToHToTauTau_M-125_8TeV-powheg-pythia6/AODSIM/PU_S10_START53_V7A-v1/0000/00E903E2-9FE9-E111-8B1E-003048FF86CA.root'
+
+                            ),
+                            skipEvents = cms.untracked.uint32(0)
+)
 
 process.output = cms.OutputModule("PoolOutputModule",
                                   outputCommands = cms.untracked.vstring('keep *'),
-                                  fileName = cms.untracked.string("testMETMVA.root")
-                                  )
+                                  fileName = cms.untracked.string("test.root")
+)
 
 process.ana      = cms.Sequence(process.pfMEtMVAsequence)
 process.p        = cms.Path(process.ana)
