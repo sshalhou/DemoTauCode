6a5cdf708cd371bab25af421e8b931a339af6328
Shalhout Shalhout <sshalhou@cern.ch>
2014-04-16 18:59:04 +0200

    input file fix

--- UserCode/TauTestAnalysis/tautestanalysis_cfg.py
+++ UserCode/TauTestAnalysis/tautestanalysis_cfg.py
@@ -6,10 +6,12 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+
+
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:/uscms/home/shalhout/1stSteps/TauID/CMSSW_5_3_12/src/MC_1.root'
+        'file:/uscms/home/shalhout/1stSteps/Git2/DemoTauCode/CMSSW_5_3_14/src/patTuple_testing.root'
     )
 )
 
