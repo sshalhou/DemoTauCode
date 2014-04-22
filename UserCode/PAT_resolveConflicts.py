2f3062bb19cdbd4705523c587b74a176d24a4f23
Shalhout Shalhout <sshalhou@cern.ch>
2014-04-22 10:07:16 +0200

    testing

--- UserCode/PAT_resolveConflicts.py
+++ UserCode/PAT_resolveConflicts.py
@@ -48,10 +48,13 @@ if not runOnMC:
   removeMCMatchingPF2PAT( process, '' )
 
 
+###################################################
+# load the PU JetID sequence
+###################################################
+process.load("CMGTools.External.pujetidsequence_cff")
 
-
-
-
+process.out.outputCommands +=['keep *_puJetId*_*_*']
+process.out.outputCommands +=['keep *_puJetMva*_*_*']
 ###################################################
 # Store the Vertex Collection
 # filtering is possible at this
@@ -69,73 +72,18 @@ process.VertexPresent = cms.EDFilter("VertexSelector",
 
 process.out.outputCommands +=['keep *_offlinePrimaryVertices*_*_*']
 
-###################################################
-# select and store the 'best' vertex
-###################################################
-
-makeTrackCandidates(process,
-    label        = 'TrackCands',
-    tracks       = cms.InputTag('generalTracks'),
-    particleType = 'pi+',
-    preselection = 'pt > 10',
-    selection    = 'pt > 10',
-    isolation    = {'tracker':0.3, 'ecalTowers':0.3, 'hcalTowers':0.3},
-    isoDeposits  = [],
-    mcAs         = 'muon'
-)
-
-## select best vertex
-process.bestVertex = cms.EDFilter(
-    "PATSingleVertexSelector",
-    mode      = cms.string("nearestToCandidate"),
-    fallbacks = cms.vstring("fromCandidate", "beamSpot"),
-    vertices              = cms.InputTag("offlinePrimaryVerticesWithBS"),
-    vertexPreselection    = cms.vstring("(chi2prob(chi2,ndf) > 0.01) && (trackSize >= 3)"),
-    candidates            = cms.VInputTag(cms.InputTag('gsfElectrons'), cms.InputTag('muons')),
-    candidatePreselection = cms.string("pt > 5"),
-    beamSpot              = cms.InputTag('offlineBeamSpot'),
-)
-
-## produce vertex associations
-process.patTrackVertexInfo = cms.EDProducer(
-    "PATVertexAssociationProducer",
-    candidates = cms.VInputTag(
-    cms.InputTag('gsfElectrons'),
-    cms.InputTag('muons'),
-    cms.InputTag('patAODTrackCands'),
-    ),
-    useTracks = cms.bool(True),
-    vertices  = cms.InputTag('bestVertex'),
-)
-
-
-# add modules to the default sequence right after the patAODTrackCands
-process.patDefaultSequence.replace(process.patAODTrackCands,
-                                   process.patAODTrackCands *
-                                   process.bestVertex *
-                                   process.patTrackVertexInfo
-                                   )
-
-
-## add it to the track candidates
-process.patTrackCands.vertexing = cms.PSet(
-    vertexAssociations = cms.InputTag("patTrackVertexInfo"),
-)
-
-## add generic tracks to the output file
-#process.out.outputCommands.append('keep *_selectedPatTrackCands_*_*')
-#process.out.outputCommands.append('keep *_patTrackVertexInfo_*_*')
-process.out.outputCommands.append('keep *_bestVertex_*_*')
 
 
 ##################################################
-# Let it run
+# Let it run, for some reason we absolutely need
+# a path called 'p'
 ###################################################
 process.p = cms.Path(        process.VertexPresent*
-                             getattr(process,"patPF2PATSequence"+postfix)
+                             getattr(process,"patPF2PATSequence"+postfix)*
+                             process.puJetIdSqeuence
                                   )
 
-#process.p+= process.VertexPresent
+
 
 if not postfix == "":
     process.p += process.recoTauClassicHPSSequence # re-run tau discriminators (new version)
