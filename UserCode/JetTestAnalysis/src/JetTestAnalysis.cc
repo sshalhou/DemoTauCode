dd4e41afc6a956809598d457bb841f98603b93bd
Shalhout Shalhout <sshalhou@cern.ch>
2014-04-24 15:25:17 +0200

    finalize prints

--- UserCode/JetTestAnalysis/src/JetTestAnalysis.cc
+++ UserCode/JetTestAnalysis/src/JetTestAnalysis.cc
@@ -122,17 +122,11 @@ for ( unsigned int i=0; i<jets->size(); ++i ) {
       float mva   = (*puJetIdMVA)[jets->refAt(i)];
       int    idflag = (*puJetIdFlag)[jets->refAt(i)];
       std::cout << "jet " << i << " pt " << patjet.pt() << " eta " << patjet.eta() << " PU JetID MVA " << mva;
-//      if( PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kLoose ) {
-  //         std::cout << " pass loose wp";
-    //  }
-//      if( PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kMedium ) {
-  //         std::cout << " pass medium wp";
-    //  }
-      //if( PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kTight ) {
-        //   std::cout << " pass tight wp";
-    //  }
 
-      std::cout<<PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kLoose );
+
+      std::cout<<" loose WP = "<<PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kLoose );
+      std::cout<<" medium WP = "<<PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kMedium );
+      std::cout<<" tight WP = "<<PileupJetIdentifier::passJetId( idflag, PileupJetIdentifier::kTight );
       std::cout << std::endl;
 }
 
