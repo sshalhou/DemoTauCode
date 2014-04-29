fed6324dd84faf008dc4c233df1c737d1f61a028
Shalhout Shalhout <sshalhou@cern.ch>
2014-04-29 15:16:07 +0200

    jec test

--- UserCode/JetTestAnalysis/src/JetTestAnalysis.cc
+++ UserCode/JetTestAnalysis/src/JetTestAnalysis.cc
@@ -184,7 +184,7 @@ for ( unsigned int i=0; i<jets->size(); ++i ) {
 
   reco::Candidate::LorentzVector uncorrJet;
   //pat::Jet const * pJet = dynamic_cast<pat::Jet const *>( &*patjet );
-  uncorrJet = patjet->correctedP4(0);
+  uncorrJet = patjet.correctedP4(0);
   std::cout<<" uncorrect jet pt = " <<uncorrJet.Pt()<<" ";
 
 
