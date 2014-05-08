ea9d2d1762c1cc6e49968db8d15746579e40640c
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-08 14:24:47 +0200

    testing simple TupleMuonProducer

--- UserCode/TupleMuonProducer/src/TupleMuonProducer.cc
+++ UserCode/TupleMuonProducer/src/TupleMuonProducer.cc
@@ -131,14 +131,14 @@ TupleMuonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   for(muon=muons->begin(); muon!=muons->end(); ++muon)
   {
 
-    TupleMuon * CurrentMuon = new TupleMuon();
+    TupleMuon CurrentMuon;
 
-    CurrentMuon->set_Pt(muon->p4().pt());
+    CurrentMuon.set_Pt(muon->p4().pt());
     cout<<" muon Pt "<<muon->p4().pt()<<endl;
     //TupleMuons->push_back(&CurrentMuon);
 
 
-    delete CurrentMuon;
+  //  delete CurrentMuon;
 
 
 
