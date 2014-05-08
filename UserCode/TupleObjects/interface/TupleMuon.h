511ca2bbf6e16f1b85d1ad843d5534a47aabb86e
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-08 13:37:58 +0200

    testing simple TupleMuon imp

--- UserCode/TupleObjects/interface/TupleMuon.h
+++ UserCode/TupleObjects/interface/TupleMuon.h
@@ -5,14 +5,6 @@
 // system include files
 #include <memory>
 
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDProducer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 // needed by ntuple muons producer
 #include <vector>
