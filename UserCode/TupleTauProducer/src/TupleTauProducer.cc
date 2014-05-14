3dc43f4e1520c3ed63bf664db465bf3b87484efe
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-14 18:07:32 +0200

    adding tau parameters

--- UserCode/TupleTauProducer/src/TupleTauProducer.cc
+++ UserCode/TupleTauProducer/src/TupleTauProducer.cc
@@ -133,10 +133,10 @@ TupleTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.getByLabel(tauSrc_,taus);
 
   // get muon collection
-  edm::Handle<edm::View<TupleMuon> > tupleMuon;
+  edm::Handle<edm::View<TupleMuonCollection> > tupleMuon;
   iEvent.getByLabel(muonSrc_,tupleMuon);
 
-  edm::View<TupleMuon>::const_iterator muon;
+  edm::View<TupleMuonCollection>::const_iterator muon;
   for(muon=tupleMuon->begin(); muon!=tupleMuon->end(); ++muon)
   {
 
