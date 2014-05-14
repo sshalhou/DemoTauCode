83b88a8ef5ce0bd78d9ed7d65c7b115b4203a32a
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-14 22:30:37 +0200

    adding tau parameters

--- UserCode/TupleTauProducer/src/TupleTauProducer.cc
+++ UserCode/TupleTauProducer/src/TupleTauProducer.cc
@@ -145,7 +145,7 @@ TupleTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
   cout<<" size "<<muons->size()<<endl;
 
-edm::View< TupleMuonCollection >::const_iterator muon;
+edm::View< TupleMuon >::const_iterator muon;
 for(muon=muons->begin(); muon!=muons->end(); ++muon)
 {
  cout<<"pdgID "<<muon->pdgId()<<endl;
