fc5fd634011c10f8a0b5168542c5f3254bb89864
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-15 18:11:24 +0200

    testing SVFit

--- UserCode/TupleMuonTauProducer/src/TupleMuonTauProducer.cc
+++ UserCode/TupleMuonTauProducer/src/TupleMuonTauProducer.cc
@@ -191,7 +191,7 @@ measuredTauLeptons.push_back(NSVfitStandalone::MeasuredTauLepton(NSVfitStandalon
 measuredTauLeptons.push_back(NSVfitStandalone::MeasuredTauLepton(NSVfitStandalone::kHadDecay, ((*taus)[j]).corrected_p4()));
 
 
-//NSVfitStandaloneAlgorithm algo(measuredTauLeptons, (*mvamet)[0].momentum(), covMET, 0);
+NSVfitStandaloneAlgorithm algo(measuredTauLeptons, (*mvamet)[0].momentum(), covMET, 0);
 //algo.addLogM(false);
 //algo.integrateMarkovChain();
 //algo.integrateVEGAS(); ////Use this instead for VEGAS integration
