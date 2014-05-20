c346821e54b2aa284ab9f1a14858a36275555502
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-20 13:48:21 +0200

    MET sys test

--- UserCode/Ntuple/ntuple_MET_cfg.py
+++ UserCode/Ntuple/ntuple_MET_cfg.py
@@ -28,11 +28,10 @@ muonSrc = cms.untracked.InputTag("selectedPatMuons")
 # needed for MVA met, but need to be here
 from JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_PAT_cfi import *
 process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
-from JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff import pfMEtMVA as pfMEtMVAtuple
-#process.load('JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff')
+process.load('JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff')
 
-process.pfMEtMVAtuple = pfMEtMVAtuple.clone(
-                    srcLeptons = cms.VInputTag("selectedPatMuons","selectedPatElectrons","selectedPatTaus")
+process.pfMEtMVAtuple = process.pfMEtMVA.clone(
+                        #  srcLeptons = cms.VInputTag("isomuons","isoelectrons","isotaus")
                                           )
 
 
