4f59cde2a3eee2e8ca1eb72791df23d329dac979
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-20 14:16:12 +0200

    MET sys test

--- UserCode/Ntuple/ntuple_MET_cfg.py
+++ UserCode/Ntuple/ntuple_MET_cfg.py
@@ -41,7 +41,7 @@ process.pfMEtMVAtuple = process.pfMEtMVA.clone(
 
 # apply type I/type I + II PFMEt corrections to pat::MET object
 # and estimate systematic uncertainties on MET
-#from PhysicsTools.PatUtils.tools.metUncertaintyTools import runMEtUncertainties
+from PhysicsTools.PatUtils.tools.metUncertaintyTools import runMEtUncertainties
 process.load("PhysicsTools.PatUtils.tools.metUncertaintyTools")
 
 runMEtUncertainties(process,
