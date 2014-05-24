801cd3b977889edf873d92d5c5ec7bdd7b30fbcc
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-24 14:51:16 +0200

    adding passFullId members to TupleMuon & TupleTau

--- UserCode/TupleMuonProducer/src/TupleMuonProducer.cc
+++ UserCode/TupleMuonProducer/src/TupleMuonProducer.cc
@@ -280,8 +280,8 @@ TupleMuonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     if(!muon->muonBestTrack().isNull())
     {
 
-      CurrentMuon.set_dz(muon->muonBestTrack()->dz());
-      CurrentMuon.set_dxy(muon->muonBestTrack()->dxy());
+      CurrentMuon.set_dz(muon->muonBestTrack()->dz(primary_vertex.position()));
+      CurrentMuon.set_dxy(muon->muonBestTrack()->dxy(primary_vertex.position()));
 
     }
 
@@ -293,7 +293,7 @@ TupleMuonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 
     CurrentMuon.set_numberOfMatchedStations(muon->numberOfMatchedStations());
-    CurrentMuon.set_dB(muon->dB());
+    CurrentMuon.set_dB(muon->dB(primary_vertex.position()));
 
 
 
@@ -366,11 +366,10 @@ TupleMuonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     cout<<" glob "<<passFullId<<endl;
     if(!(muon->isTightMuon(primary_vertex))) passFullId = 0;
     cout<<" tight "<<passFullId<<endl;
-    if(!(fabs(muon->dB()) < 0.045)) passFullId = 0;
-    cout<<" dB "<<passFullId<<" val "<<muon->dB()<<endl;
-    if(!(fabs(muon->muonBestTrack()->dz()) < 0.2)) passFullId = 0;
-    cout<<" dz "<<passFullId<<" val "<<muon->muonBestTrack()->dz()<<endl;
-    cout<<" dz2 "<<passFullId<<" val "<<muon->muonBestTrack()->dz(primary_vertex.position())<<endl;
+    if(!(fabs(muon->dB(primary_vertex.position())) < 0.045)) passFullId = 0;
+    cout<<" dB "<<passFullId<<" val "<<muon->dB(primary_vertex.position())<<endl;
+    if(!(fabs(muon->muonBestTrack()->dz(primary_vertex.position())) < 0.2)) passFullId = 0;
+    cout<<" dz "<<passFullId<<" val "<<muon->muonBestTrack()->dz(primary_vertex.position())<<endl;
 
     if(!(irel_DR4 < 0.1)) passFullId = 0;
     if(!(muon->p4().pt()>20)) passFullId = 0;
