52fc7fcf063af867a3f0d70a6a82bc3ebd3e6795
Shalhout Shalhout <sshalhou@cern.ch>
2014-05-30 16:28:48 +0200

    fix

--- UserCode/PAT_producer.py
+++ UserCode/PAT_producer.py
@@ -217,7 +217,7 @@ process.load("Configuration.Geometry.GeometryPilot2_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
 
-process.load("CommonTools.ParticleFlow.pfTaus_cff")
+#process.load("CommonTools.ParticleFlow.pfTaus_cff")
 
 from PhysicsTools.PatAlgos.tools.tauTools import *
 switchToPFTauHPS(process)
