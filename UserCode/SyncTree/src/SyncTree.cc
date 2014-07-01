c87a31338100399f74ae50731bb9bb9e717f444d
Shalhout Shalhout <sshalhou@cern.ch>
2014-07-01 11:44:37 +0200

    adding skeleton for sync analysis

--- UserCode/SyncTree/src/SyncTree.cc
+++ UserCode/SyncTree/src/SyncTree.cc
@@ -2,13 +2,13 @@
 //
 // Package:    SyncTree
 // Class:      SyncTree
-// 
+//
 /**\class SyncTree SyncTree.cc TEMP/SyncTree/src/SyncTree.cc
 
- Description: [one line class summary]
+Description: [one line class summary]
 
- Implementation:
-     [Notes on implementation]
+Implementation:
+[Notes on implementation]
 */
 //
 // Original Author:  shalhout shalhout
@@ -24,34 +24,54 @@
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "UserCode/TupleObjects/interface/TupleElectrons.h"
+#include "UserCode/TupleObjects/interface/TupleMuons.h"
+#include "UserCode/TupleObjects/interface/TupleTaus.h"
+#include "UserCode/TupleObjects/interface/TupleElectronTaus.h"
+#include "UserCode/TupleObjects/interface/TupleMuonTaus.h"
+
+
+#include <string>
+#include "TTree.h"
+#include "TFile.h"
+
+using namespace edm;
+using namespace std;
+
+typedef math::XYZTLorentzVector LorentzVector;
+
 //
 // class declaration
 //
 
 class SyncTree : public edm::EDAnalyzer {
-   public:
-      explicit SyncTree(const edm::ParameterSet&);
-      ~SyncTree();
+public:
+  explicit SyncTree(const edm::ParameterSet&);
+  ~SyncTree();
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 
-   private:
-      virtual void beginJob() ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
+private:
+  virtual void beginJob() ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
 
-      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-      virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+  virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+  virtual void endRun(edm::Run const&, edm::EventSetup const&);
+  virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+  virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
 
-      // ----------member data ---------------------------
+  // ----------member data ---------------------------
+
+edm::InputTag tauSrc_;
+edm::InputTag leptonSrc_;
+edm::InputTag leptonTauSrc_;
+string NAME_;
+
 };
 
 //
@@ -65,19 +85,22 @@ class SyncTree : public edm::EDAnalyzer {
 //
 // constructors and destructor
 //
-SyncTree::SyncTree(const edm::ParameterSet& iConfig)
-
+SyncTree::SyncTree(const edm::ParameterSet& iConfig):
+tauSrc_(iConfig.getParameter<edm::InputTag>("tauSrc" )),
+leptonSrc_(iConfig.getParameter<edm::InputTag>("leptonSrc" )),
+leptonTauSrc_(iConfig.getParameter<edm::InputTag>("leptonTauSrc" )),
+NAME_(iConfig.getParameter<string>("NAME" ))
 {
-   //now do what ever initialization is needed
+  //now do what ever initialization is needed
 
 }
 
 
 SyncTree::~SyncTree()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
+
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
 
 }
 
@@ -90,54 +113,54 @@ SyncTree::~SyncTree()
 void
 SyncTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  using namespace edm;
 
 
 
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+  #ifdef THIS_IS_AN_EVENT_EXAMPLE
+  Handle<ExampleData> pIn;
+  iEvent.getByLabel("example",pIn);
+  #endif
+
+  #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+  ESHandle<SetupData> pSetup;
+  iSetup.get<SetupRecord>().get(pSetup);
+  #endif
 }
 
 
 // ------------ method called once each job just before starting event loop  ------------
-void 
+void
 SyncTree::beginJob()
 {
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
-void 
-SyncTree::endJob() 
+void
+SyncTree::endJob()
 {
 }
 
 // ------------ method called when starting to processes a run  ------------
-void 
+void
 SyncTree::beginRun(edm::Run const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when ending the processing of a run  ------------
-void 
+void
 SyncTree::endRun(edm::Run const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when starting to processes a luminosity block  ------------
-void 
+void
 SyncTree::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when ending the processing of a luminosity block  ------------
-void 
+void
 SyncTree::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
 {
 }
--- UserCode/SyncTree/synctree_cfg.py
+++ UserCode/SyncTree/synctree_cfg.py
@@ -9,11 +9,15 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        'file:/uscms/home/shalhout/no_backup/NtupleFile_susy120_first250000.root'
     )
 )
 
-process.demo = cms.EDAnalyzer('SyncTree'
+process.demo = cms.EDAnalyzer('SyncTree',
+tauSrc = cms.InputTag('TupleTausNominal','TupleTausNominal','Ntuple'),
+leptonSrc = cms.InputTag('TupleMuonsNominal','TupleMuonsNominal','Ntuple'),
+leptonTauSrc = cms.InputTag('TupleMuonTausNominal','TupleMuonTausNominal','Ntuple'),
+NAME = cms.string("muTau") # only muTau or eTau
 )
 
 
