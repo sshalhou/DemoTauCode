4f8677700927bc7d0dda0a393d2e4a535628df56
Shalhout Shalhout <sshalhou@cern.ch>
2014-08-11 12:52:11 +0200

    testing electron isolation

--- UserCode/ElectronTestAnalysis/src/ElectronTestAnalysis.cc
+++ UserCode/ElectronTestAnalysis/src/ElectronTestAnalysis.cc
@@ -150,7 +150,7 @@ for(edm::View<pat::Electron>::const_iterator electron=electrons->begin(); electr
 AbsVetos  vetos2012EBPFIdCharged;
 ConeVeto AXZ(Direction(electron->eta(),electron->phi()),0.010);
 
-//vetos2012EBPFIdCharged.push_back(AXZ);
+vetos2012EBPFIdCharged.push_back(AXZ);
 
 
 /*
