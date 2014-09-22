ef09a01c0c3428030eb6a2aef60d7ffa804a64b8
Shalhout Shalhout <sshalhou@cern.ch>
2014-09-22 15:45:24 +0200

    test

--- UserCode/ConfigFileGenerator/generate_configs.py
+++ UserCode/ConfigFileGenerator/generate_configs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import os
 
 
 if len(sys.argv) is 2:
@@ -15,28 +16,36 @@ else:
   sys.exit()
 
 
+fileNameXML=os.environ['CMSSW_BASE']+"/src/UserCode/ConfigFileGenerator/test.xml"
+print fileNameXML
+
+
+
 import xml.etree.ElementTree as ET
-tree = ET.parse('test.xml')
+tree = ET.parse(fileNameXML)
 root = tree.getroot()
 
-#for child in root:
-#  print child.tag, child.attrib
 
 interested_in = str(sys.argv[1])
 
-for element in tree.iterfind('Sample[@SampleName="/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM"]'):
+for element in root.findall('Sample'):
   SampleName = element.get("SampleName")
-  PhysicsProcess = element.get("PhysicsProcess")
-  CrossSection = element.get("CrossSection")
-  Mass = element.get("Mass")
-  isNonTopEmbeddedSample = element.get("isNonTopEmbeddedSample")
-  isTopEmbeddedSample = element.get("isTopEmbeddedSample")
-  runOnMC = element.get("runOnMC")
-  Events = element.get("Events")
-  Files = element.get("Files")
-  SizeTB = element.get("SizeTB")
-  print "Mass = ", Mass
-  print "SizeTB = ", SizeTB
-
-from subprocess import call
-call(['./das_client.py','--query="file dataset=/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM" --verbose=1'])
+  if interested_in == SampleName:
+    PhysicsProcess = element.get("PhysicsProcess")
+    CrossSection = element.get("CrossSection")
+    Mass = element.get("Mass")
+    isNonTopEmbeddedSample = element.get("isNonTopEmbeddedSample")
+    isTopEmbeddedSample = element.get("isTopEmbeddedSample")
+    runOnMC = element.get("runOnMC")
+    Events = element.get("Events")
+    Files = element.get("Files")
+    SizeTB = element.get("SizeTB")
+    print "Mass = ", Mass
+    print "SizeTB = ", SizeTB
+    WhiteListCommand=os.environ['CMSSW_BASE']+"/src/UserCode/ConfigFileGenerator/"
+    WhiteListCommand+="das_client.py --query=\"site dataset="
+    WhiteListCommand+=interested_in
+    WhiteListCommand+="\" --verbose=1 | egrep \"T2|T3\" | awk \'{printf $1\" \"}\'"
+    getWhiteList = os.popen(WhiteListCommand)
+    whiteList = getWhiteList.read()
+    print "list = ", whiteList
