b3d879437e31cada654481bf8f000d18afeff505
Shalhout Shalhout <sshalhou@cern.ch>
2014-09-29 16:44:16 +0200

    adding python cfi

--- UserCode/FlatTuple/src/FlatTuple.cc
+++ UserCode/FlatTuple/src/FlatTuple.cc
@@ -181,14 +181,10 @@ FlatTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     }
 
 
-  ///////////
-  // fill the tree
+    ///////////
+    // fill the tree
 
-  lepTauTree->Fill();
-
-
-
-}
+    lepTauTree->Fill();
 
 
 
@@ -207,67 +203,71 @@ FlatTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 
 
-  #ifdef THIS_IS_AN_EVENT_EXAMPLE
-  Handle<ExampleData> pIn;
-  iEvent.getByLabel("example",pIn);
-  #endif
-
-  #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-  ESHandle<SetupData> pSetup;
-  iSetup.get<SetupRecord>().get(pSetup);
-  #endif
-}
 
 
-// ------------ method called once each job just before starting event loop  ------------
-void
-FlatTree::beginJob()
-{
-}
-
-// ------------ method called once each job just after ending the event loop  ------------
-void
-FlatTree::endJob()
-{
-  lepTauTree->Write();
-  outFile->Close();
 
 
-}
+    #ifdef THIS_IS_AN_EVENT_EXAMPLE
+    Handle<ExampleData> pIn;
+    iEvent.getByLabel("example",pIn);
+    #endif
 
-// ------------ method called when starting to processes a run  ------------
-void
-FlatTree::beginRun(edm::Run const&, edm::EventSetup const&)
-{
-}
+    #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+    ESHandle<SetupData> pSetup;
+    iSetup.get<SetupRecord>().get(pSetup);
+    #endif
+  }
 
-// ------------ method called when ending the processing of a run  ------------
-void
-FlatTree::endRun(edm::Run const&, edm::EventSetup const&)
-{
-}
 
-// ------------ method called when starting to processes a luminosity block  ------------
-void
-FlatTree::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
+  // ------------ method called once each job just before starting event loop  ------------
+  void
+  FlatTree::beginJob()
+  {
+  }
 
-// ------------ method called when ending the processing of a luminosity block  ------------
-void
-FlatTree::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
+  // ------------ method called once each job just after ending the event loop  ------------
+  void
+  FlatTree::endJob()
+  {
+    lepTauTree->Write();
+    outFile->Close();
 
-// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
-void
-FlatTree::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
-}
 
-//define this as a plug-in
-DEFINE_FWK_MODULE(FlatTree);
+  }
+
+  // ------------ method called when starting to processes a run  ------------
+  void
+  FlatTree::beginRun(edm::Run const&, edm::EventSetup const&)
+  {
+  }
+
+  // ------------ method called when ending the processing of a run  ------------
+  void
+  FlatTree::endRun(edm::Run const&, edm::EventSetup const&)
+  {
+  }
+
+  // ------------ method called when starting to processes a luminosity block  ------------
+  void
+  FlatTree::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+  {
+  }
+
+  // ------------ method called when ending the processing of a luminosity block  ------------
+  void
+  FlatTree::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+  {
+  }
+
+  // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+  void
+  FlatTree::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+    //The following says we do not know what parameters are allowed so do no validation
+    // Please change this to state exactly what you do use, even if it is no parameters
+    edm::ParameterSetDescription desc;
+    desc.setUnknown();
+    descriptions.addDefault(desc);
+  }
+
+  //define this as a plug-in
+  DEFINE_FWK_MODULE(FlatTree);
