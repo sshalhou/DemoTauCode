f1c9101b3a6604b73191a27871b40a7e6c6aba03
Shalhout Shalhout <sshalhou@cern.ch>
2014-10-01 19:31:48 +0200

    adding some cuts on e, mu, and tau, (via FullID) in order to eliminate time-out ntuple jobs, to turn these on run Ntuple with doNotRequireFullIdForLeptons set to False

--- UserCode/Analysis/Configurations/python/eventRequirements.py
+++ UserCode/Analysis/Configurations/python/eventRequirements.py
@@ -5,12 +5,49 @@ from ROOT import gROOT,TChain, TLorentzVector, TSelector, TTree, TF1, TH1F, TCan
 
 
 
-def muonTrigger(chain, index, useNewTriggers):
-  returnVal = False
-  if chain.muT_muon_has_HltMatchMu17[index] is True:
-    returnVal = True
-  if chain.muT_muon_has_HltMatchMu18[index] is True:
-    returnVal = True
-  if chain.muT_muon_has_HltMatchMu24[index] is True and useNewTriggers is True:
-    returnVal = True
+def pairCutsMuTau(chain, index):
+  returnVal = True
+  if chain.muT_sumCharge[index] != 0:
+    returnVal = False
+    return returnVal
+  if chain.muT_DR[index] <= 0.5:
+    returnVal = False
+    return returnVal
+  if chain.muT_passesTriLeptonVeto[index] is not True:
+    returnVal = False
+    return returnVal
   return returnVal;
+
+def pairCutsETau(chain, index):
+  returnVal = True
+  if chain.eT_sumCharge[index] != 0:
+    returnVal = False
+    return returnVal
+  if chain.eT_DR[index] <= 0.5:
+    returnVal = False
+    return returnVal
+  if chain.eT_passesTriLeptonVeto[index] is not True:
+    returnVal = False
+    return returnVal
+  return returnVal;
+
+##############
+# in cases with
+# multiple good H candidates
+# find the maxPt pair
+
+def getMaxPtPairIndex(chain, maxPairTypeAndIndex, passingMuTauIndices, passingETauIndices):
+  currentMaxPt = -999.
+  for index in passingMuTauIndices:
+    if chain.muT_scalarSumPt[index] > currentMaxPt:
+      currentMaxPt = chain.muT_scalarSumPt[index]
+      del maxPairTypeAndIndex[:]
+      maxPairTypeAndIndex.append(index)
+      maxPairTypeAndIndex.append('muonTau')
+  for index in passingETauIndices:
+    if chain.eT_scalarSumPt[index] > currentMaxPt:
+      del maxPairTypeAndIndex[:]
+      currentMaxPt = chain.eT_scalarSumPt[index]
+      maxPairTypeAndIndex.append(index)
+      maxPairTypeAndIndex.append('electronTau')
+  return
--- UserCode/Analysis/run_analysis.py
+++ UserCode/Analysis/run_analysis.py
@@ -17,6 +17,9 @@ chain = TChain('demo/FlatTuple')
 listOfFiles = []
 listOfFiles.append('./FlatTuple_eTau.root')
 
+
+#listOfFiles.append('/Users/shalhout/Desktop/CMS_ANALYSIS/scratch/120_SYNC/FlatAna/FlatTuple_eTau.root')
+
 for afile in listOfFiles:
 	for index in range(0,100):
 		treeName = 'demo/FlatTuple;'+str(index)
@@ -36,6 +39,7 @@ from Configurations.python.electronID import *
 from Configurations.python.muonID import *
 from Configurations.python.tauID import *
 from Configurations.python.triggerRequirements import *
+from Configurations.python.eventRequirements import *
 
 
 
@@ -46,11 +50,40 @@ for entry in range(0,chain.GetEntries()):
 		localEntry = chain.LoadTree(entryNumber);
 		if (localEntry >= 0):
 			selector.Process(localEntry)
+			passingETauIndices = []
+			passingMuTauIndices = []
+			maxPairTypeAndIndex = []
+
+#########################################################
+# loop over eTau pairs and store all that pass in list
+
 			for index in range(0, chain.eT_correctedSVFitMass.size()):
-				if electronID(chain,index,Verbose) is True and tauID_eTau(chain, index,Verbose) is True:
+				if electronID(chain,index,Verbose) and tauID_eTau(chain, index,Verbose) and pairCutsETau(chain, index):
 					if electronTrigger(chain,index,UseNewTriggers) is True and tauTriggerForETau(chain, index, UseNewTriggers) is True:
-						print 'PASS eTau'
+						#print 'PASS eTau'
+						passingETauIndices.append(index)
+
+
+#########################################################
+# loop over muTau pairs and store all that pass in list
+
 			for index in range(0, chain.muT_correctedSVFitMass.size()):
-				if muonID(chain,index,Verbose) is True and tauID_muTau(chain, index, Verbose) is True:
+				if muonID(chain,index,Verbose) and tauID_muTau(chain, index, Verbose) and pairCutsMuTau(chain, index):
 					if muonTrigger(chain,index,UseNewTriggers) is True and tauTriggerForMuTau(chain, index, UseNewTriggers) is True:
-						print 'PASS muTau'
+						#print 'PASS muTau'
+						passingMuTauIndices.append(index)
+
+#########################################################
+# if have multiple passing H candidates in the same event
+# find the highest sumPt pair
+
+			if (len(passingETauIndices) + len(passingMuTauIndices)) == 1:
+				if len(passingETauIndices) == 1 :
+					maxPairTypeAndIndex.append(passingETauIndices[0])
+					maxPairTypeAndIndex.append('electronTau')
+				elif len(passingMuTauIndices) == 1:
+					maxPairTypeAndIndex.append(passingMuTauIndices[0])
+					maxPairTypeAndIndex.append('muonTau')
+			if (len(passingETauIndices) + len(passingMuTauIndices)) > 1:
+					getMaxPtPairIndex(chain, maxPairTypeAndIndex, passingMuTauIndices, passingETauIndices)
+			print 'PASS', maxPairTypeAndIndex
