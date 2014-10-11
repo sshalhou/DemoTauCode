eef01e2d0061bf389b27a232f25d064694d6d0cf
Shalhout Shalhout <sshalhou@cern.ch>
2014-10-11 13:00:06 +0200

    test

--- UserCode/TEMP/test.py
+++ UserCode/TEMP/test.py
@@ -1,7 +1,75 @@
-import FWCore.ParameterSet.Config as cms
 ########################################################################################################
+import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-from PhysicsTools.PatAlgos.tools.helpers import *
+########################################################################################################
+
+
+SampleName_='SUSYGluGluToHToTauTau_M-120_8TeV-pythia6-tauola/AODSIM/PU_S10_START53_V7A-v1/AODSIM'
+PhysicsProcess_='ggA0tautau[SUSY_120_8TeV]'
+MASS_=120.0
+isNonTopEmbeddedSample_ = False
+isTopEmbeddedSample_ = False
+runOnMC = True # true for MC, and all topTopBar and Ztautau embedded samples
+branchingFraction_ = 999.99
+crossSection_ = 999.99
+numberEvents_ = 999
+
+FilterEvents = True
+DropSelectedPatObjects = True
+KeepAll = False
+PrintProductIDs = False
+
+###################################################
+# Store SampleName and PhysicsProcess
+# eventually these should be command line
+# options
+###################################################
+
+process.UserSpecifiedData = cms.EDProducer('TupleUserSpecifiedDataProducer',
+                                            SampleName=cms.string(SampleName_),
+                                            PhysicsProcess=cms.string(PhysicsProcess_),
+                                            isNonTopEmbeddedSample=cms.bool(isNonTopEmbeddedSample_),
+                                            isTopEmbeddedSample=cms.bool(isTopEmbeddedSample_),
+                                            MASS=cms.double(MASS_),
+                                            crossSection=cms.double(crossSection_),
+                                            branchingFraction=cms.double(branchingFraction_),
+                                            numberEvents=cms.int32(numberEvents_)
+                                            )
+
+
+
+
+
+##################################################
+# Let it run
+###################################################
+process.p = cms.Path(process.UserSpecifiedData)
+
+
+
+########################################################################################################
+if KeepAll:
+  process.out.outputCommands +=['keep *_*_*_*']
+########################################################################################################
+process.out.fileName = '/uscms/home/shalhout/no_backup/patTuple_testing.root'
+
+########################################################################################################
+myfilelist = cms.untracked.vstring()
+myfilelist.extend(['file:/uscms/home/shalhout/no_backup/oneThousand_selectedEventsRaw.root'])
+
+process.source = cms.Source ("PoolSource",
+                      fileNames=myfilelist,
+                        dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+                        inputCommands = cms.untracked.vstring(
+                        'keep *',
+                        'drop recoPFTaus_*_*_*'
+                        ),
+                      skipEvents=cms.untracked.uint32(0)
+                             )
+########################################################################################################
+
+
+process.maxEvents.input = 20
 ########################################################################################################
