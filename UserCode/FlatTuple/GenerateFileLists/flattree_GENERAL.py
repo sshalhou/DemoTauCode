df1ce056c9f613950e7aea082b80da17c6f0cf2b
Shalhout Shalhout <sshalhou@cern.ch>
2014-10-21 15:32:36 +0200

    using NAME in FlatTuple to track TauES, adding scripts for fileList generation

--- UserCode/FlatTuple/GenerateFileLists/flattree_GENERAL.py
+++ UserCode/FlatTuple/GenerateFileLists/flattree_GENERAL.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("FlatTuple")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
+
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
