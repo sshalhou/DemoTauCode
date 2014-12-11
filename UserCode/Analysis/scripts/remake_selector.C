6daed99ceba2d6ca3d86d74fbda0581e04ff97fd
Shalhout Shalhout <sshalhou@cern.ch>
2014-12-11 18:56:30 +0100

    MSSM added new anti-e mva for muTau btag and ZImpact cut for eTau

--- UserCode/Analysis/scripts/remake_selector.C
+++ UserCode/Analysis/scripts/remake_selector.C
@@ -1,6 +1,6 @@
 {
 
-TFile *_file0 = TFile::Open("../testCombined.root");
+TFile *_file0 = TFile::Open("/uscms/home/shalhout/no_backup/NEWSKIM/VBFHToTauTauM125_v9FlatTuple/FlatTuple_22_1_1Nq_skimmed.root");
 TTree * FlatTree = (_file0).Get("TauEsNominal/FlatTuple");
 FlatTree->MakeSelector("FlatTreeSel");
 gROOT->ProcessLine(Form(".! sed -i -e 's/return kTRUE\;/fChain->GetTree\(\)->GetEntry\(entry\)\; return kTRUE\;/g' FlatTreeSel.C"));
