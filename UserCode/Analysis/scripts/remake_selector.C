82904f488ab30ad0f686cac32c92706d6c0909f1
Shalhout Shalhout <sshalhou@cern.ch>
2015-01-29 18:53:19 +0100

    updating stitching for W, DY, and DY low mass

--- UserCode/Analysis/scripts/remake_selector.C
+++ UserCode/Analysis/scripts/remake_selector.C
@@ -1,7 +1,7 @@
 {
 
 //TFile *_file0 = TFile::Open("/uscms/home/shalhout/no_backup/NEWSKIM/VBFHToTauTauM125_v9FlatTuple/FlatTuple_22_1_1Nq_skimmed.root");
-TFile *_file0 = TFile::Open("~/no_backup/NEWSKIMV9X/nMSSM_bba1tautaum80_FLATv9Xntup/FlatTuple_11_1_S7e_skimmed.root");
+TFile *_file0 = TFile::Open("~/no_backup/NEWSKIMV10/ZTauTauEmbeddedETauA_FlatTupeV10/FlatTuple_1_1_0VF_skimmed.root");
 TTree * FlatTree = (_file0).Get("TauEsNominal/FlatTuple");
 FlatTree->MakeSelector("FlatTreeSel");
 gROOT->ProcessLine(Form(".! sed -i -e 's/return kTRUE\;/fChain->GetTree\(\)->GetEntry\(entry\)\; return kTRUE\;/g' FlatTreeSel.C"));
--- UserCode/AnalysisNMSSM/scripts/remake_selector.C
+++ UserCode/AnalysisNMSSM/scripts/remake_selector.C
@@ -1,7 +1,7 @@
 {
 
 //TFile *_file0 = TFile::Open("../HOLDER/Signal25_FlatTuple_10_1_wrN_skimmed.root");
-TFile *_file0 = TFile::Open("~/no_backup/NEWSKIMV9X/nMSSM_bba1tautaum80_FLATv9Xntup/FlatTuple_11_1_S7e_skimmed.root");
+TFile *_file0 = TFile::Open("~/no_backup/NEWSKIMV10/ZTauTauEmbeddedETauA_FlatTupeV10/FlatTuple_1_1_0VF_skimmed.root");
 TTree * FlatTree = (_file0).Get("TauEsNominal/FlatTuple");
 FlatTree->MakeSelector("FlatTreeSel");
 gROOT->ProcessLine(Form(".! sed -i -e 's/return kTRUE\;/fChain->GetTree\(\)->GetEntry\(entry\)\; return kTRUE\;/g' FlatTreeSel.C"));
